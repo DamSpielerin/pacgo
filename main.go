636e628df6285f0f38caab1cebcb983b847ec2f5
alona.maksymova <alona.maksymova@care.com>
2021-11-05 14:36:38 +0200

    Finished :seven: :foot:

--- main.go
+++ main.go
@@ -1,28 +1,301 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"log"
+	"math/rand"
+	"os"
+	"os/exec"
+	"time"
+
+	"github.com/danicat/simpleansi"
+)
+
+type sprite struct {
+	row int
+	col int
+}
+type Config struct {
+	Player   string `json:"player"`
+	Ghost    string `json:"ghost"`
+	Wall     string `json:"wall"`
+	Dot      string `json:"dot"`
+	Pill     string `json:"pill"`
+	Death    string `json:"death"`
+	Space    string `json:"space"`
+	UseEmoji bool   `json:"use_emoji"`
+}
+
+var cfg Config
+var player sprite
+var ghosts []*sprite
+var score int
+var numDots int
+var lives = 1
+var maze []string
+
+func loadConfig(file string) error {
+	f, err := os.Open(file)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	decoder := json.NewDecoder(f)
+	err = decoder.Decode(&cfg)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func loadMaze(file string) error {
+	f, err := os.Open(file)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		line := scanner.Text()
+		maze = append(maze, line)
+	}
+
+	for row, line := range maze {
+		for col, char := range line {
+			switch char {
+			case 'P':
+				player = sprite{row, col}
+			case 'G':
+				ghosts = append(ghosts, &sprite{row, col})
+			case '.':
+				numDots++
+			}
+
+		}
+	}
+
+	return nil
+}
+
+func printScreen() {
+	simpleansi.ClearScreen()
+	for _, line := range maze {
+		for _, chr := range line {
+			switch chr {
+			case '#':
+				fmt.Print(simpleansi.WithBlueBackground(cfg.Wall))
+			case '.':
+				fmt.Print(cfg.Dot)
+			case 'X':
+				fmt.Print(cfg.Pill)
+			default:
+				fmt.Print(cfg.Space)
+			}
+		}
+		fmt.Println()
+	}
+	moveCursor(player.row, player.col)
+	fmt.Print(cfg.Player)
+	for _, g := range ghosts {
+		moveCursor(g.row, g.col)
+		fmt.Print(cfg.Ghost)
+	}
+
+	//Move cursor outside of maze drawing area
+	moveCursor(len(maze)+1, 0)
+	fmt.Println("Score:", score, "\tLives:", lives)
+}
+
+func initialise() {
+	cbTerm := exec.Command("stty", "cbreak", "-echo")
+	cbTerm.Stdin = os.Stdin
+
+	err := cbTerm.Run()
+	if err != nil {
+		log.Fatalln("unable to activate cbreak mode:", err)
+	}
+}
+
+func cleanup() {
+	cookedTerm := exec.Command("stty", "-cbreak", "echo")
+	cookedTerm.Stdin = os.Stdin
+
+	err := cookedTerm.Run()
+	if err != nil {
+		log.Fatalln("unable to restore cooked mode:", err)
+	}
+}
+
+func readInput() (string, error) {
+	buffer := make([]byte, 100)
+
+	cnt, err := os.Stdin.Read(buffer)
+	if err != nil {
+		return "", err
+	}
+
+	if cnt == 1 && buffer[0] == 0x1b {
+		return "ESC", nil
+	} else if cnt >= 3 {
+		switch buffer[2] {
+		case 'A':
+			return "UP", nil
+		case 'B':
+			return "DOWN", nil
+		case 'C':
+			return "RIGHT", nil
+		case 'D':
+			return "LEFT", nil
+		}
+
+	}
+	return "", nil
+}
+
+func makeMove(oldRow, oldCol int, dir string) (newRow, newCol int) {
+	newRow, newCol = oldRow, oldCol
+	switch dir {
+	case "UP":
+		newRow -= 1
+		if newRow < 0 {
+			newRow = len(maze) - 1
+		}
+	case "DOWN":
+		newRow += 1
+		if newRow == len(maze) {
+			newRow = 0
+		}
+	case "RIGHT":
+		newCol += 1
+		if newCol == len(maze[0]) {
+			newCol = 0
+		}
+	case "LEFT":
+		newCol -= 1
+		if newCol < 0 {
+			newCol = len(maze[0]) - 1
+		}
+	}
+
+	if maze[newRow][newCol] == '#' {
+		newRow, newCol = oldRow, oldCol
+	}
+
+	return
+}
+
+func movePlayer(dir string) {
+	player.row, player.col = makeMove(player.row, player.col, dir)
+	removeDot := func(row, col int) {
+		maze[row] = maze[row][0:col] + " " + maze[row][col+1:]
+	}
+	switch maze[player.row][player.col] {
+	case '.':
+		numDots--
+		score++
+		removeDot(player.row, player.col)
+	case 'X':
+		score += 10
+		removeDot(player.row, player.col)
+	}
+
+}
+
+func moveCursor(row, col int) {
+	if cfg.UseEmoji {
+		simpleansi.MoveCursor(row, col*2)
+	} else {
+		simpleansi.MoveCursor(row, col)
+	}
+}
+
+func drawDirection() string {
+	dir := rand.Intn(4)
+	move := map[int]string{
+		0: "UP",
+		1: "DOWN",
+		2: "RIGHT",
+		3: "LEFT",
+	}
+	return move[dir]
+}
+
+func moveGhosts() {
+	for _, g := range ghosts {
+		dir := drawDirection()
+		g.row, g.col = makeMove(g.row, g.col, dir)
+	}
+}
 
 func main() {
 	// initialize game
-
+	initialise()
+	defer cleanup()
 	// load resources
+	err := loadMaze("maze01.txt")
+	if err != nil {
+		log.Println("failed to load maze:", err)
+	}
+	err = loadConfig("config.json")
+	if err != nil {
+		log.Println("failed to load configuration:", err)
+		return
+	}
+	//process input (async)
+	input := make(chan string)
+	go func(ch chan<- string) {
+		for {
+			input, err := readInput()
+			if err != nil {
+				log.Println("error reading input:", err)
+				ch <- "ESC"
+			}
+			ch <- input
+		}
+	}(input)
 
 	// game loop
 	for {
-		// update screen
-
-		// process input
-
 		// process movement
+		var inp string
+		select {
+		case inp = <-input:
+			if inp == "ESC" {
+				lives = 0
+			}
+			movePlayer(inp)
+		default:
+
+		}
+		moveGhosts()
 
 		// process collisions
+		for _, g := range ghosts {
+			if player == *g {
+				lives = 0
+			}
+		}
+		// update screen
+		printScreen()
 
 		// check game over
+		if numDots == 0 || lives <= 0 {
+			if lives == 0 {
+				moveCursor(player.row, player.col)
+				fmt.Print(cfg.Death)
+				moveCursor(len(maze)+2, 0)
+			}
+			break
+		}
 
 		// Temp: break infinite loop
-		fmt.Println("Hello, Pac Go!")
-		break
 
 		// repeat
+		time.Sleep(200 * time.Millisecond)
 	}
 }
